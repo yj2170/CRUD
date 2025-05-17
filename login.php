087f669484da7327bdd8a66cfe53e191c73a2635
yj2170 <a01021701734@gmail.com>
2025-05-17 16:32:15 +0900

    1st

---
+++ login.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Login</title>
+</head>
+
+<body>
+    <h5><u><a href="http://localhost/CRUD/login.php">home</a></u>
+    <u><a href="http://localhost/CRUD/register.php">register</a></u>
+    <u><a href="http://localhost/CRUD/login.php">login</a></u></h5>
+
+    <h3><strong>login</strong></h3>
+
+    <!-- login-->
+
+</body>
+
+</html>
---
+++ register.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Register</title>
+</head>
+
+<body>
+    <h5><u><a href="http://localhost/CRUD/login.php">home</a></u>
+    <u><a href="http://localhost/CRUD/register.php">register</a></u>
+    <u><a href="http://localhost/CRUD/login.php">login</a></u></h5>
+
+    <h3><strong>register</strong></h3>
+</body>
+
+</html>
